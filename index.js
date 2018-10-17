a4dd55e28da79d3f1847605de1dd33c4754ebd83
MuathAlawi Alawi <muathalawi1@gmail.com>
2018-10-17 00:52:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let currentUser = 'Muath'
 
 let  welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
 
-let excitedWelcomeMessage = 'Welcome to Flatbook'
+let excitedWelcomeMessage = 'Welcome to Flatbook, ' + currentUser.toUpperCase() + '!'
+
 let shortGreeting = 'Welcome, ' + currentUser.charAt(0) + '!'
 
